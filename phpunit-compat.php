b0e5d0cd1e9b21884284fbcc8892d5c33f9b5231
Cesar Rodas <cesar@rodas.me>
2017-05-03 18:34:18 -0400

    Make phpunit 6.0 compatible with older phpunits

--- phpunit-compat.php
+++ phpunit-compat.php
@@ -12,6 +12,15 @@ function phpunit_compat_autoloader($class) {
         $type       = $reflection->isAbstract() ? 'abstract class' : 'class';
         eval("$type $class extends $new_class {}");
     }
+    if (preg_match('@^phpunit\\\\@i', $class)) {
+        $namespace  = explode("\\", $class);
+        $ns_class   = array_pop($namespace);
+        $namespace  = implode("\\", $namespace);
+        $new_class  = str_replace("\\", "_", $class);
+        $reflection = new ReflectionClass($new_class);
+        $type       = $reflection->isAbstract() ? 'abstract class' : 'class';
+        eval("namespace $namespace { $type $ns_class extends $new_class {} }");
+    }
 }
 
 spl_autoload_register('phpunit_compat_autoloader');
